08be312ae6497d75c4f1b4ef0836c520f1662bb1
aybjax <aybat93@gmail.com>
2023-04-28 21:15:56 +0600

    .

--- cmntypes/queue.go
+++ cmntypes/queue.go
@@ -3,7 +3,7 @@ package cmntypes
 import "github.com/aybjax/nis_lib/pbdto"
 
 type AppQueue interface {
-	TransformAndNotify(*pbdto.DiffIds) error
+	SendToNotify(*pbdto.DiffIds) error
 	Notify(*pbdto.UpdateEmbedded) error
 	AddListener(func(*pbdto.UpdateEmbedded) error)
 }
