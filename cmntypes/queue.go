46c2ce3a5e89214faa349483f396039df9cb877c
aybjax <aybat93@gmail.com>
2023-04-29 01:18:33 +0600

    .

--- cmntypes/queue.go
+++ cmntypes/queue.go
@@ -1,6 +1,6 @@
 package cmntypes
 
 type AppQueue interface {
-	Enqueue(data []byte, queue string, topic string) error
-	Dequeue(queue string, topic string, cb func(data []byte) error)
+	Publish(data []byte, queue string, topic string) error
+	Subscribe(queue string, topic string, cb func(data []byte) error)
 }
